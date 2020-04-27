45c5bd0e62d73c879f3132fb070e0b434194a5ec
Starkus1999 <thalisson.bandeira@hotmail.com>
2020-04-27 17:16:34 -0500

    Frontend: continuaçao da NavBar

--- frontend/src/Pages/Dashboard/Index.js
+++ frontend/src/Pages/Dashboard/Index.js
@@ -5,6 +5,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
 import InitialFooter from '../../Component/InitialFooter';
 import Typography from '@material-ui/core/Typography';
+import PersonIcon from '@material-ui/icons/Person';
+import AccountBalanceOutlinedIcon from '@material-ui/icons/AccountBalanceOutlined';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 
 //PAGES
 import Home from './Home';
@@ -15,12 +18,35 @@ const useStyles = makeStyles((theme) => ({
     },
     leftSide: {
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        '& img': {
+            width: '7.5vh',
+            marginRight: 5
+        },
+        [theme.breakpoints.down('sm')]: {
+            backgroundColor: '#247BA0',
+            color: '#FFF',
+            margin: '-20px -10px 20px -10px',
+            padding: 3,
+            display: 'flex',
+            justifyContent: 'center'
+        }
+    },
+    NavBarButtons: {
+        color: '#FFF',
+        [theme.breakpoints.down('sm')]: {
+            display: 'flex',
+            color: '#000',
+            margin: '5px 0',
+            '& span svg': {
+                paddingRight: 5
+            }
+        }
     }
 }));
 
 const MenuLeft = (props) => {
-    const classes = useStyles()
+    const classes = useStyles();
     return (
         <div className={classes.leftSide}>
             <img src="images/Logo.svg" alt="logo" className="logo" />
@@ -32,11 +58,13 @@ const MenuLeft = (props) => {
 }
 
 const Menu = (props) => {
+    const classes = useStyles();
     return (
         <>
-            <Button>teste</Button>
-            <Button>teste2</Button>
-            <Button>teste3</Button>
+            <Button className={classes.NavBarButtons}><AccountBalanceOutlinedIcon />Instituições</Button>
+            <Button className={classes.NavBarButtons}><FavoriteBorderIcon />Minhas Doações</Button>
+            <Button className={classes.NavBarButtons}><PersonIcon /></Button>
+            
         </>
     )
 }
@@ -44,6 +72,16 @@ const Menu = (props) => {
 
 export default function Dashboard() {
     const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     return(
         <div>
