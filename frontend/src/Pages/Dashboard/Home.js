929b7d74afa80d2f30274b90ca6e409a6039be50
Starkus1999 <thalisson.bandeira@hotmail.com>
2020-04-27 15:18:44 -0500

    Frontend: adicionado Navbar do MaterialKIT

--- frontend/src/Pages/Dashboard/Home.js
+++ frontend/src/Pages/Dashboard/Home.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import NavBar from '../../Component/MaterialKit/NavBarHeader/Header';
 
 export default function Home() {
     return(
-        <h1>Dashboard</h1>
+        <Grid container>
+            <NavBar
+            color="info"
+            rightLinks={
+                <ul>
+                    <li>Link1</li>
+                    <li>Link2</li>
+                </ul>
+            } />
+            <h1>Dash</h1>
+        </Grid>
     );
 }
