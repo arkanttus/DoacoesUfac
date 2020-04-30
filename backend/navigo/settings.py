2397b73f80eb9e1972cf5d6eeb82f129acad4027
Bruno Trindade <bruuno.trindade@gmail.com>
2020-04-29 21:54:24 -0500

    update settings

--- backend/navigo/settings.py
+++ backend/navigo/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'navigo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.mysql' if PROD else 'django.db.backends.postgresql_psycopg2',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASS'),
