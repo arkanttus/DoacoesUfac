207bf5e5b053a2178fce555bb5d04db0888fd48c
Jhonatan Patrocinio <jhonatan.patrocinio@gmail.com>
2020-05-01 15:17:16 -0500

    feature: intitution donate endpoint

--- backend/apps/base/api/viewsets.py
+++ backend/apps/base/api/viewsets.py
@@ -1,13 +1,22 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, response
+from rest_framework.decorators import action
 
 from .serializers import InstitutionReadSerializer, TypeInstitutionSerializer
 from apps.base.models import Institution, TypeInstitution
+from apps.donates.models import Donate
+from apps.donates.serializers import DonateSerializer
 
 
 class InstitutionView(viewsets.ModelViewSet):
     queryset = Institution.objects.all()
     serializer_class = InstitutionReadSerializer
     permission_classes = (permissions.AllowAny,)
+
+    @action(methods=['get'], detail=True)
+    def donates(self, request, pk=None):
+        queryset = Donate.objects.filter(institution_id=pk)
+        serializer = DonateSerializer(queryset, many=True)
+        return response.Response(serializer.data)
 
 
 class TypeInstitutionView(viewsets.ModelViewSet):
