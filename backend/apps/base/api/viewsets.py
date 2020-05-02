979a681450303bf442a4d3f89dbe204d8c31fbf0
Jhonatan Patrocinio <jhonatan.patrocinio@gmail.com>
2020-05-01 19:54:18 -0500

    fix: update users, feature: add method need_donates on institution

--- backend/apps/base/api/viewsets.py
+++ backend/apps/base/api/viewsets.py
@@ -5,6 +5,8 @@ from .serializers import InstitutionReadSerializer, TypeInstitutionSerializer
 from apps.base.models import Institution, TypeInstitution
 from apps.donates.models import Donate
 from apps.donates.serializers import DonateSerializer
+from apps.need_donate.models import NeedDonate
+from apps.need_donate.serializers import NeedDonateSerializer
 
 
 class InstitutionView(viewsets.ModelViewSet):
@@ -16,6 +18,12 @@ class InstitutionView(viewsets.ModelViewSet):
     def donates(self, request, pk=None):
         queryset = Donate.objects.filter(institution_id=pk)
         serializer = DonateSerializer(queryset, many=True)
+        return response.Response(serializer.data)
+
+    @action(methods=['get'], detail=True)
+    def need_donates(self, request, pk=None):
+        queryset = NeedDonate.objects.filter(institution_id=pk)
+        serializer = NeedDonateSerializer(queryset, many=True)
         return response.Response(serializer.data)
 
 
--- backend/apps/users/api/viewsets.py
+++ backend/apps/users/api/viewsets.py
@@ -37,7 +37,22 @@ class UserView(viewsets.ModelViewSet):
         return user
 
     def update(self, request, *args, **kwargs):
-        return super(UserView, self).update(request, *args, **kwargs)
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+        try:
+            qs = User.objects.get(id=instance.id)
+            serializer_read = UserReadSerializer(qs)
+        except User.DoesNotExist:
+            return response.Response({'errors': _('Usuário não encontrado')}, status=status.HTTP_404_NOT_FOUND)
+        return response.Response(serializer_read.data)
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
