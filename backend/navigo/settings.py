9bc9f976ed7d2ce38f16ffb81dfd01c678edfe4f
Jhonatan Patrocinio <jhonatan.patrocinio@gmail.com>
2020-05-05 21:41:45 -0500

    fix: settings

--- backend/navigo/settings.py
+++ backend/navigo/settings.py
@@ -90,20 +90,7 @@ DATABASES = {
     }
 }
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+AUTH_PASSWORD_VALIDATORS = []
 
 AUTH_USER_MODEL = 'users.User'
 
