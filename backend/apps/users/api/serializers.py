f53510b92203fc5f715cb11f4e707b286bd3f89f
Jhonatan Patrocinio <jhonatan.patrocinio@gmail.com>
2020-05-05 22:07:15 -0500

    fix: settings

--- backend/apps/users/api/serializers.py
+++ backend/apps/users/api/serializers.py
@@ -92,42 +92,24 @@ class PasswordChangeSerializer(serializers.Serializer):
     new_password1 = serializers.CharField(max_length=128)
     new_password2 = serializers.CharField(max_length=128)
 
-    set_password_form_class = SetPasswordForm
-
     def __init__(self, *args, **kwargs):
-        self.old_password_field_enabled = getattr(
-            settings, 'OLD_PASSWORD_FIELD_ENABLED', False
-        )
         super(PasswordChangeSerializer, self).__init__(*args, **kwargs)
-
-        if not self.old_password_field_enabled:
-            self.fields.pop('old_password')
 
         self.request = self.context.get('request')
         self.user = getattr(self.request, 'user', None)
 
     def validate_old_password(self, value):
-        invalid_password_conditions = (
-            self.old_password_field_enabled,
-            self.user,
-            not self.user.check_password(value)
-        )
-
-        if all(invalid_password_conditions):
+        if not self.user and not self.user.check_password(value):
             raise serializers.ValidationError('Senha invalida')
         return value
 
     def validate(self, attrs):
-        self.set_password_form = self.set_password_form_class(
-            user=self.user, data=attrs
-        )
-
-        if not self.set_password_form.is_valid():
-            raise serializers.ValidationError(self.set_password_form.errors)
+        if self.new_password1 != self.new_password2:
+            raise serializers.ValidationError('Senhas não iguais')
         return attrs
 
     def save(self):
-        self.set_password_form.save()
+        self.user.set_password(self.new_password1)
 
 
 
