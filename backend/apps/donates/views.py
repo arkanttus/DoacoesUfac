206450bdf16bef9101149104df56a8cccaa79d41
Bruno Trindade <bruuno.trindade@gmail.com>
2020-05-10 10:13:36 -0500

    Recaptcha adicionado

--- backend/apps/donates/views.py
+++ backend/apps/donates/views.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets, permissions
+
+from rest_framework import viewsets, permissions, response, status
 
 from .serializers import DonateSerializer, DonateUpdateSerializer
 from .models import Donate
 from .permissions import *
+from .utils import validate_recaptcha
 
 from apps.base.models import Institution
 from apps.users.models import User
@@ -38,6 +40,25 @@ class DonateView(viewsets.ModelViewSet):
             return DonateUpdateSerializer
         return DonateSerializer
     
+    def create(self, request, *args, **kwargs):
+        serializer = DonateSerializer(data=request.data, context=self.get_serializer_context())
+        serializer.is_valid(raise_exception=True)
+
+        try:
+            print(request.data)
+            recaptcha_response = request.data.get('g-recaptcha-response')
+            print(recaptcha_response)
+            if validate_recaptcha(recaptcha_response):
+                self.perform_create(serializer)
+            else:
+                return response.Response({'errors': 'Recaptcha inválido'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        except Institution.DoesNotExist:
+            return response.Response({'errors': 'Recaptcha inválido'}, status=status.HTTP_401_UNAUTHORIZED)
+            
+        headers = self.get_success_headers(serializer.data)
+        return response.Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
     def perform_create(self, serializer):
         donator = self.request.user
         serializer.save(donator=donator)
